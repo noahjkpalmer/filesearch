273dd73c251db2a9d184995eb08e3f8bc163d445
Noah Palmer <noahjkpalmer@gmail.com>
2019-04-04 12:17:45 -0700

    updated logger

--- main.py
+++ main.py
@@ -11,21 +11,28 @@ import json
 #Module Imports
 from FileSearcher import FileSearcher
 
+def log(data):
+	logfile = open("log.txt", "a")
+	logfile.write(data + "\n")
+	logfile.close()
+	print(data)
+
+
 def main():
-	directorypath = "/media/hd_internal-02/www/liquidweb"#input("Directory Path: ")
+	directorypath = "../oldsite"#"/media/hd_internal-02/www/liquidweb"#input("Directory Path: ")
 	filesearcher = FileSearcher(directorypath)
 	passwords = None
 	with open("passwords.json") as f:
 		passwords = json.load(f)
 	for passtype in passwords.keys():
 		locations = filesearcher.search(passwords[passtype])
-		print("Password Type: ", passtype)
+		log("Password Type: " + passtype)
 		if not bool(locations):
-			print("Password is not present in directory!")
+			log("Password is not present in directory!")
 		else:
 			for key in locations.keys():
-				print(key, locations[key])
-		print("")
+				log("Path: " + key + " | Lines: " + str(locations[key]))
+		log("")
 
 if __name__ == '__main__':
 	main()
